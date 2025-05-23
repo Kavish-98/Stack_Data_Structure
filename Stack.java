63a2f49142c79b04fe1f74ccc368e9590395748a
Kavish-98 <vimansha98@gmail.com>
2025-05-23 15:41:05 +0530

    insert values

--- Stack.java
+++ Stack.java
@@ -8,9 +8,29 @@ public class Stack {
         capacity = size;
         top = -1;
     }
+
+    boolean isFull(){
+        return top+1 == capacity;
+    }
+
+   void push(int x){
+        if(isFull()){
+            System.out.println("over flow");
+        }
+        else{
+            top++;
+            arr[top] = x;
+            System.out.println("Inserting: "+ x);
+        }
+    }
     public static void main(String[] args){
         Stack myStack = new Stack(5);
-        
 
+        myStack.push(10);
+        myStack.push(20);
+        myStack.push(30);
+        myStack.push(40);
+        myStack.push(50);
+        myStack.push(60);
     }
 }

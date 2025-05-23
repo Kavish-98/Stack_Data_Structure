10bfa80f4e53923d5d3cd439d6c651542d700311
Kavish-98 <vimansha98@gmail.com>
2025-05-23 15:21:41 +0530

    Create Basic Stack

---
+++ Stack.java
@@ -0,0 +1,16 @@
+public class Stack {
+    int arr[];
+    int top;
+    int capacity;
+    
+    Stack(int size){
+        arr = new int[size];
+        capacity = size;
+        top = -1;
+    }
+    public static void main(String[] args){
+        Stack myStack = new Stack(5);
+        
+
+    }
+}
